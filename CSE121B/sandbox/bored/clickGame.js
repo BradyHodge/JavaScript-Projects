b6f1d5c4cae740eb754903940aaebe2792cf290e
Brady Hodge <bradyhodge1@gmail.com>
2023-10-10 13:23:07 -0600

    fixed +2 point problem, add style and bg img,

--- CSE121B/sandbox/bored/clickGame.js
+++ CSE121B/sandbox/bored/clickGame.js
@@ -1,24 +1,33 @@
 let score = 0;
 let timeLeft = 10;
-let gameInterval;
 let timerInterval;
+let gameStarted = false;
 
 function startGame() {
+    if (gameStarted) return;
+
+    gameStarted = true;
     score = 0;
     timeLeft = 10;
     document.getElementById('score').textContent = score;
     document.getElementById('timer').textContent = timeLeft;
-    gameInterval = setInterval(moveCircle, 1000);
     timerInterval = setInterval(updateTimer, 1000);
 }
 
-function circleClicked() {
-    score++;
-    document.getElementById('score').textContent = score;
-    moveCircle();
+function circleClicked(event) {
+    if (!gameStarted) {
+        startGame();
+    } else {
+        score++;
+        document.getElementById('score').textContent = score;
+        moveCircle(event);
+    }
 }
 
-function moveCircle() {
+function moveCircle(event) {
+    if (event) {
+        event.stopPropagation();
+    }
     const circle = document.querySelector('.circle');
     const x = Math.random() * (400 - 50);
     const y = Math.random() * (400 - 50);
@@ -30,8 +39,10 @@ function updateTimer() {
     timeLeft--;
     document.getElementById('timer').textContent = timeLeft;
     if (timeLeft <= 0) {
-        clearInterval(gameInterval);
         clearInterval(timerInterval);
+        gameStarted = false;
         alert('Game over! Your score is: ' + score);
     }
 }
+
+document.querySelector('.circle').addEventListener('click', circleClicked);
